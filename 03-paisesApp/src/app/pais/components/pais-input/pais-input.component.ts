7baf739a0fb1e149e7efd75505956ad81f72e421
Saul Paez <saul.paez@gmail.com>
2022-02-13 21:50:56 -0600

    #Fin Domingo

---
+++ 03-paisesApp/src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,25 @@
+import { outputAst } from '@angular/compiler';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-pais-input',
+  templateUrl: './pais-input.component.html',
+  styles: [
+  ]
+})
+export class PaisInputComponent implements OnInit {
+
+  @Output() onEnter: EventEmitter<string> = new EventEmitter();
+
+  termino: string = '';
+
+  buscar(){
+    this.onEnter.emit( this.termino );
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
